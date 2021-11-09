0bb26087630cfa8c0d5cb294c8f044ad16b5f2c6
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-11-09 19:29:10 +0700

    Update kartu_antrian.php

--- application/views/kartu_antrian.php
+++ application/views/kartu_antrian.php
@@ -127,7 +127,7 @@ POS SATPAM DAN LANGSUNG MENUJU HRD PERUSAHAAN MASING2</h6>
     </div>
 </div>
 <center>
-           <a class="btn btn-warning mt-5" id="btn-Convert-Html2Image" href="#"><span class="fa fa-download"></span> Download No Urut</a> 
+           <a class="btn btn-warning mt-3" id="btn-Convert-Html2Image" href="#"><span class="fa fa-download"></span> Download No Urut</a> 
 </center>
            <div id="previewImage" style="display:none"></div> <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
